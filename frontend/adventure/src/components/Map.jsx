9d485aecf404e952ea3bdf4d7bbe01e6c6b10646
Rajoza123 <ozaraj76@gmail.com>
2024-08-22 14:44:05 +0530

    Added map

--- frontend/adventure/src/components/Map.jsx
+++ frontend/adventure/src/components/Map.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
- const Map = () => {
+// Fix for default marker icon issue
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const MyMap = (props) => {
+
+  
+  const {location, cordinates} = props
   return (
-    <>
-      <div id="map"></div>
-    </>
-  )
-}
- 
-export default Map;
+    <MapContainer
+      center={cordinates}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={cordinates}>
+        <Popup>
+          {location}
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MyMap;
