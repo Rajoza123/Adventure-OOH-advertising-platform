ed3a7aec60e2791b9b39554ddbbeed595aef29fc
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-08-23 21:33:43 +0530

    cd

--- frontend/adventure/src/components/Main.jsx
+++ frontend/adventure/src/components/Main.jsx
@@ -24,20 +24,20 @@ const Main = () => {
         <Container>
           <Nav className=" ms-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="Publisher/">Publisher</Nav.Link>
-            <Nav.Link href="#pricing">Company</Nav.Link>
-            <Nav.Link href="#pricing">About Us</Nav.Link>
-            <Nav.Link href="#pricing">Login</Nav.Link>
-            <Nav.Link href="#pricing">Signup</Nav.Link>
+            <Nav.Link href="/Publisher">Publisher</Nav.Link>
+            <Nav.Link href="/company">Company</Nav.Link>
+            <Nav.Link href="/aboutus">About Us</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/signup">Signup</Nav.Link>
           </Nav>
         </Container>
         </Navbar>
         <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/AboutUs" element={<About/>}></Route>
-            <Route path="/ContactUs" element={<Contact/>}></Route>
-            <Route path="/Login" element={<Login/>}></Route>
-            <Route path="/Map" element={<Map/>}></Route>
+            <Route path="/aboutus" element={<About/>}></Route>
+            <Route path="/contactus" element={<Contact/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/map" element={<Map/>}></Route>
             <Route path="/Publisher" element={<Publisher/>}></Route>
         </Routes>
       </Router>
