563a7b8f7f6ae0e1ed40f168dd345c0aa0a80bed
DhruvaAhir <dhruvahir143@gmail.com>
2024-08-26 16:58:39 +0530

    "Updated serializers and views in billboard module, added new serializer for companies, and modified database."

--- backend/adventure/billboard/serializer.py
+++ backend/adventure/billboard/serializer.py
@@ -1,21 +1,32 @@
 from rest_framework import serializers 
 from . models import *
-from publisher.models import publishers
+from publisher.serializer import *
+from company.serializer import *
 
 
 
-class ReactBillBoardSerializer(serializers.ModelSerializer): 
-	class Meta: 
-		model = billboards
-		fields = ('__all__')
- 	
-
+class CompanySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = companies
+        fields = ('__all__')
+        
 class ReactBillBoardTypeSerializer(serializers.ModelSerializer): 
 	class Meta: 
 		model = billboard_type
 		fields = ('__all__')
 
+class ReactBillBoardSerializer(serializers.ModelSerializer):
+	class Meta: 
+		model = billboards
+		fields = ('__all__')
+	# publisher_id = ReactPublisherSerializer(read_only=True)
+	# type = ReactBillBoardTypeSerializer(read_only=True)
+ 	
+
 class ReactBillxCompSerializer(serializers.ModelSerializer): 
+	# billboard_id = ReactBillBoardSerializer(read_only=True)
+	# company_id = CompanySerializer(read_only=True)
 	class Meta: 
 		model = billxcomp
 		fields = ('__all__') 
