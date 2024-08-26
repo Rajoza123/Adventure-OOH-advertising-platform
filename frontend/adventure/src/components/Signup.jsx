67c267f36ec7ba1947ec10aac3cc76fae808b8a3
Rajoza123 <ozaraj76@gmail.com>
2024-08-27 00:43:25 +0530

    signup usestate

--- frontend/adventure/src/components/Signup.jsx
+++ frontend/adventure/src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -10,9 +10,25 @@ import {
   MDBIcon,
   MDBCheckbox
 }
+
 from 'mdb-react-ui-kit';
+import axios from 'axios';
 
 function Signup() {
+  const[data,setdata] = useState({})
+
+  const handlesubmit = ()=>{
+          const homedata = document.forms['signup']
+          setdata({'name':homedata.name,'email':homedata.email,'contact':homedata.contact,'image':homedata.img})
+          axios.post('http://localhost:8000/company/',homedata)
+          .then((res)=>{
+            console.log(res)
+          })
+          .catch((er)=>{
+            console.log(er)
+          })
+  }
+
   return (
     <MDBContainer fluid>
 
@@ -24,17 +40,18 @@ function Signup() {
 
               <h2 className="fw-bold mb-2 text-center">Sign Up</h2>
               <p className="text-white-50 mb-3">Please enter your login and password!</p>
-              <MDBInput wrapperClass='mb-4 w-100' label='Name' id='formControlLg' type='text' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg" maxLength={10}/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Contact No.' id='formControlLg' type='number' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Image' id='formControlLg' type='file' size="lg"/>
+              <form name='signup'>
+
+              <MDBInput wrapperClass='mb-4 w-100' label='Name' id='formControlLg1' type='text' size="lg" name='name'/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg2' type='email' size="lg" name='email'/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg3' type='password' size="lg" maxLength={10} name='Pass'/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Contact No.' id='formControlLg4' type='number' size="lg" name='Contact'/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Image' id='formControlLg5' type='file' size="lg" name='img'/>
 
               <MDBCheckbox name='flexCheck' id='flexCheckDefault' className='mb-4' label='Remember password' />
 
-              <MDBBtn size='lg'>
-                Signup
-              </MDBBtn>
+              <button className=' btn btn-primary' onClick={handlesubmit}>Signup</button>
+              </form>
             <a href="/Publisher_signup" className='text-primary'>signup as Publisher</a>
               <hr className="my-4" />
 
