692a93907d190634b0532eb14a469e1beafdc1f8
DhruvaAhir <dhruvahir143@gmail.com>
2024-09-15 16:17:19 +0530

    "Updated company model to make email unique, added login and signup functionality to frontend, and modified database"

--- frontend/adventure/src/components/Login.jsx
+++ frontend/adventure/src/components/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Button, Container, Row, Col, Card, Form } from 'react-bootstrap';
 import { Facebook, Twitter, Google } from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function Login() {
+
+  const [currentUser, setCurrentUser] = useState('publisher')
+
+  const handleSubmit = async ()=>{
+    const formdata = new FormData()
+    const form = document.forms['signin']
+    formdata.append('email', form.email.value)
+    formdata.append('password', form.password.value)
+
+
+    try {
+              const response = await axios.post('http://127.0.0.1:8000/comp_signin/', formdata, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+    
+                // Handle successful sign-in
+                localStorage.setItem('company_id', response.data.id);
+                localStorage.setItem('company_name', response.data.name);
+                localStorage.setItem('is_authenticated', true);
+                localStorage.setItem('token',response.data.token);
+
+                // Redirect to a different page, e.g., dashboard
+                window.location.href = '/company';
+            } catch (error) {
+                alert(error)
+    }
+}
   return (
     <Container fluid className="vh-100 d-flex justify-content-center align-items-center">
       <Row className="d-flex justify-content-center align-items-center h-100">
@@ -12,22 +42,22 @@ function Login() {
             <Card.Body className="p-5 d-flex flex-column align-items-center mx-auto w-100">
               <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
               <p className="text-white-50 mb-5">Please enter your login and password!</p>
-
+              <form name='signin'>
               <Form.Group className="mb-4 w-100" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" className="bg-dark text-white" />
+                <Form.Control type="email" name='email' placeholder="Enter email" className="bg-dark text-white" />
               </Form.Group>
 
               <Form.Group className="mb-4 w-100" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" className="bg-dark text-white" />
+                <Form.Control type="password" name='password' placeholder="Enter password" className="bg-dark text-white" />
               </Form.Group>
 
               <p className="small mb-3">
                 <a className="text-white-50" href="#!">Forgot password?</a>
               </p>
 
-              <Button variant="outline-light" size="lg" className="mb-4">
+              <Button variant="outline-light" size="lg" className="mb-4" onClick={handleSubmit}>
                 Login
               </Button>
 
@@ -42,7 +72,7 @@ function Login() {
                   <Google size={28} />
                 </Button>
               </div>
-
+              </form>
               <p className="mb-0">
                 Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a>
               </p>
