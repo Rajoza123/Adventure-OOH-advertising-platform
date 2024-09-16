2a1605e876b1eb0bc887e9e12095ca2b56b344e3
DhruvaAhir <dhruvahir143@gmail.com>
2024-09-16 15:31:18 +0530

    "Updated backend and frontend code: modified models, serializers, and admin files in backend; updated Login and Publisher_signup components in frontend."

--- backend/adventure/publisher/admin.py
+++ backend/adventure/publisher/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import publishers
+from .models import *
 from company.models import CompanyAuthToken as companyAuth
 # Register your models here.
 admin.site.register(publishers)
 admin.site.register(companyAuth)
+admin.site.register(PublisherAuthToken)
--- frontend/adventure/src/components/Publisher_signup.jsx
+++ frontend/adventure/src/components/Publisher_signup.jsx
@@ -1,51 +1,118 @@
-import React from 'react';
-import { Button, Container, Row, Col, Card, Form } from 'react-bootstrap';
-import { Facebook, Twitter, Google } from 'react-bootstrap-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { Button, Container, Row, Col, Card, Form } from "react-bootstrap";
+import { Facebook, Twitter, Google } from "react-bootstrap-icons";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Publisher_signup() {
+  const [data, setdata] = useState({});
+
+  const handlesubmit = () => {
+    const homedata = document.forms["signup"];
+    const formdata = new FormData();
+    formdata.append("name", homedata.name.value);
+    formdata.append("email", homedata.email.value);
+    formdata.append("contact", homedata.contact.value);
+    formdata.append("password", homedata.password.value);
+    formdata.append("image", homedata.img.files[0]);
+
+    axios
+      .post("http://localhost:8000/publisher/", formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status == 201) {
+          alert("Account Created Successfully");
+          window.location.assign("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <Container fluid className="vh-100 d-flex justify-content-center align-items-center">
+    <Container
+      fluid
+      className="vh-100 d-flex justify-content-center align-items-center"
+    >
       <Row className="d-flex justify-content-center align-items-center h-100">
         <Col md={6} lg={12}>
-          <Card className="bg-dark text-white my-5 mx-auto" style={{ borderRadius: '1rem', maxWidth: '500px' }}>
+          <Card
+            className="bg-dark text-white my-5 mx-auto"
+            style={{ borderRadius: "1rem", maxWidth: "500px" }}
+          >
             <Card.Body className="p-5 d-flex flex-column align-items-center mx-auto w-100">
               <h2 className="fw-bold mb-2 text-uppercase">Sign Up</h2>
-              <p className="text-white-50 mb-5">Please enter your details to create an account!</p>
+              <p className="text-white-50 mb-5">
+                Please enter your details to create an account!
+              </p>
+              <form name="signup">
+                <Form.Group className="mb-4 w-100" controlId="formBasicName">
+                  <Form.Label>Full Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your name"
+                    name="name"
+                    className="bg-dark text-white"
+                  />
+                </Form.Group>
 
-              {/* Full Name */}
-              <Form.Group className="mb-4 w-100" controlId="formBasicName">
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" className="bg-dark text-white" />
-              </Form.Group>
+                {/* Email Address */}
+                <Form.Group className="mb-4 w-100" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    name="email"
+                    className="bg-dark text-white"
+                  />
+                </Form.Group>
 
-              {/* Email Address */}
-              <Form.Group className="mb-4 w-100" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" className="bg-dark text-white" />
-              </Form.Group>
+                <Form.Group
+                  className="mb-4 w-100"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Confirm password"
+                    name="password"
+                    className="bg-dark text-white"
+                  />
+                </Form.Group>
 
-              {/* Password */}
-              <Form.Group className="mb-4 w-100" controlId="formBasicPassword">
-                <Form.Label>Contact</Form.Label>
-                <Form.Control type="number" placeholder="Enter password" className="bg-dark text-white" />
-              </Form.Group>
+                <Form.Group className="mb-4 w-100" controlId="formBasicContact">
+                  <Form.Label>Contact</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter password"
+                    name="contact"
+                    className="bg-dark text-white"
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-4 w-100" controlId="formBasicPassword">
-                <Form.Label>Image</Form.Label>
-                <Form.Control type="file" placeholder="Enter password" className="bg-dark text-white" />
-              </Form.Group>
+                <Form.Group className="mb-4 w-100" controlId="formBasicimg">
+                  <Form.Label>Image</Form.Label>
+                  <Form.Control
+                    type="file"
+                    placeholder="Enter password"
+                    name="img"
+                    className="bg-dark text-white"
+                  />
+                </Form.Group>
 
-              {/* Confirm Password */}
-              <Form.Group className="mb-4 w-100" controlId="formBasicConfirmPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm password" className="bg-dark text-white" />
-              </Form.Group>
-
-              <Button variant="outline-light" size="lg" className="mb-4">
-                Sign Up
-              </Button>
-
+                <Button
+                  variant="outline-light"
+                  size="lg"
+                  className="mb-4"
+                  onClick={handlesubmit}
+                >
+                  Sign Up
+                </Button>
+              </form>
               <div className="d-flex flex-row justify-content-center mb-5">
                 <Button variant="link" className="text-white p-2">
                   <Facebook size={28} />
@@ -59,11 +126,17 @@ function Publisher_signup() {
               </div>
 
               <p className="mb-0">
-                Signup as company <a href="/Signup" className="text-white-50 fw-bold">Signup</a>
+                Signup as publisher{" "}
+                <a href="/Publisher_signup" className="text-white-50 fw-bold">
+                  Signup
+                </a>
               </p>
 
               <p className="mb-0">
-                Already have an account? <a href="/login" className="text-white-50 fw-bold">Login</a>
+                Already have an account?{" "}
+                <a href="#!" className="text-white-50 fw-bold">
+                  Login
+                </a>
               </p>
             </Card.Body>
           </Card>
@@ -73,4 +146,4 @@ function Publisher_signup() {
   );
 }
 
-export default  Publisher_signup;
+export default Publisher_signup;
