4c80f57fd5967847f34e31826c1eb6cb8920fe84
DhruvaAhir <dhruvahir143@gmail.com>
2024-09-22 18:15:25 +0530

    "Removed commented out code and a merge conflict marker in BillxCompView and Bookings classes"

--- backend/adventure/billboard/views.py
+++ backend/adventure/billboard/views.py
@@ -60,7 +60,6 @@ class BillxCompView(APIView):
 	# 		serializer.save() 
 	# 		return Response(serializer.data) 
 
-<<<<<<< HEAD
 class Bookings(APIView):
 
     def get(self, request):
@@ -110,10 +109,6 @@ class Bookings(APIView):
 
         # Return the combined serialized data
         return Response(data, status=status.HTTP_200_OK)
-=======
-# class Bookings(APIView):
-#     def get(self,request):
->>>>>>> 27fc297375cf9fd9b1fe61c41742c3e3e5824d0d
         
 
 
